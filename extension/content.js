1bd61c8a3e01aba56f7509e1f86774730902ee49
ClarityV1 <ori_cyrille@hotmail.com>
2026-04-21 22:25:49 +0000

    commit please

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,19 @@
-console.log("Apprentice is watching...");
+console.log("WorkLess Apprentice: I am awake and watching.");
+
+// This listens for your mouse clicks and identifies WHAT you clicked
+document.addEventListener('click', (event) => {
+    const element = event.target;
+    
+    // We capture the 'Path' to the button so we can find it again later
+    const metadata = {
+        tag: element.tagName,
+        text: element.innerText || element.value,
+        id: element.id,
+        classes: element.className,
+        timestamp: new Date().toLocaleTimeString()
+    };
+
+    console.log("Apprentice saw you click:", metadata);
+    
+    // Later, this will send the data to your 'Brain' website
+});
